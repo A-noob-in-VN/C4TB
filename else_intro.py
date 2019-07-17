f491693154d18cdf8a8999fae1eddb40e9cd5b41
namdj100 <52542123+namdj100@users.noreply.github.com>
2019-07-17 16:23:06 +0700

    Add files via upload

---
+++ else_intro.py
@@ -0,0 +1,12 @@
+season = int(input("What month?"))
+
+print(season)
+
+if 0 < season < 4:
+    print ("Spring")
+elif 3 < season < 7:
+    print ("summer")
+elif 6 < season < 10:
+    print ("Autumn")
+elif 9 < season < 13:
+    print ("Winter")
