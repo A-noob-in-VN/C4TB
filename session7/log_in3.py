64fc6f4ad275978e49e37ef86b8f62d7e7d32936
namdj100 <52542123+namdj100@users.noreply.github.com>
2019-07-26 22:58:58 +0700

    Add files via upload

--- session7/log_in3.py
+++ session7/log_in3.py
@@ -1,16 +1,18 @@
 username = input("Your username?")
 print (username)
-
-import re
-
 email = input("Your email address?")
 print (email)
-if re.search:
-    print ("Must have punctuation")
-else:
-    pass
-password = input("Your password?")
-print (password)
+while True:
+    password = input("Your password?")
+    print (password)
+    minimum_character = 8
+    passlength = len(password)
+    if passlength < minimum_character:
+        print (Must have at least 8 characters)
+    elif password.isalpha:
+        print (Must include numbers)
+    else:
+        break
 request = input("Enter your password again")
 print (request)
 if request != password:
