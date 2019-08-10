3e7b2ae8a18e943c3edfed3b38e0e60d2c3096dd
namdj100 <52542123+namdj100@users.noreply.github.com>
2019-08-10 20:25:47 +0700

    Add files via upload

---
+++ session9/Part5/DN.py
@@ -0,0 +1,10 @@
+district = ["ST","BĐ","BTL","CG","ĐĐ","HBT"]
+populations = [150300,247100,333300,266800,420900,318000]
+a = max(populations)
+b = min(populations)
+print (populations.index(a))
+print (populations.index(b))
+c = populations.index(a)
+d = populations.index(b)
+print (district[c])
+print (district[d])
