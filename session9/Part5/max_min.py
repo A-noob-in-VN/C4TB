cd77a29650946d846c7fde1d8a47a88f25ea05db
namdj100 <52542123+namdj100@users.noreply.github.com>
2019-08-10 19:54:13 +0700

    Update max_min.py

--- session9/Part5/max_min.py
+++ session9/Part5/max_min.py
@@ -1,4 +1,6 @@
 district = ["ST","BĐ","BTL","CG","ĐĐ","HBT"]
 population = [150300,247100,333300,266800,420900,318000]
-print("Highest population: ",max(population))
-print("Lowest population: ",min(population))
+a = max(population)
+b = min(population)
+print (population.index(a))
+print (population.index(b))
